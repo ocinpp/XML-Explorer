7ececc28bbea707c760c85ff27a83086e7f06890
ocinpp <ocin25@gmail.com>
2020-03-08 13:38:15 +0800

    Update test

--- test/util/parser.test.js
+++ test/util/parser.test.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 describe("parser", () => {
   test("parseXml returns correct result for XML file", async () => {
-    const result = await parser.parseXml(__dirname + path.sep + "test.xml");
+    const result = await parser.parseXmlFile(__dirname + path.sep + "test.xml");
     const expected = [
       "note/body",
       "note/from",
@@ -15,7 +15,7 @@ describe("parser", () => {
   });
 
   test("parseXml returns null for test file", async () => {
-    const result = await parser.parseXml(__dirname + path.sep + "test.txt");
+    const result = await parser.parseXmlFile(__dirname + path.sep + "test.txt");
     expect(result).toBeNull();
   });
 });
